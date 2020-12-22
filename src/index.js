202afed6d9c9e29d240997892c710de06df842fe
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-22 12:54:47 -0300

    feat: update response type

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const http = require('http');
 
+const users = require('./mocks/users');
+
 // Criando servidor HTTP
 const server = http.createServer((request, response) => {
   console.log(`Request Method: ${request.method} | Endpoint: ${request.url}`);
 
+  if(request.url === '/users' && request.method === 'GET'){
+    response.writeHead(200, { 
+      'Content-Type': 'application/json', 
+    });
+    response.end('<h1>Ola Mundo!</h1>');
+  }
+
   // Escrevendo informações no cabeçalho
-  response.writeHead(200, { 
-    'Content-Type': 'text/html', 
-  });
-  response.end('<h1>Ola Mundo!</h1>');
+  // response.writeHead(200, { 
+  //   'Content-Type': 'text/html', 
+  // });
+  // response.end('<h1>Ola Mundo!</h1>');
 });
 
 server.listen(3001, () => {
