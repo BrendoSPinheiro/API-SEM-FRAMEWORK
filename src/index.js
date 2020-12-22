1888672aff724b79bf8b45456436e945ff4a0450
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-22 12:27:47 -0300

    feat: create server

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const http = require('http');
 
 // Criando servidor HTTP
 const server = http.createServer((request, response) => {
+  // Escrevendo informações no cabeçalho
   response.writeHead(200, { 
     'Content-Type': 'text/html', 
   });
   response.end('<h1>Hello World!</h1>');
+});
+
+server.listen(3001, () => {
+  console.log('🔥 Server started at http://localhost:3001');
 });
