7df26108d38b8b23fae08b2cf780b0c8db2fc619
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-22 12:51:18 -0300

    feat: create users mock

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const http = require('http');
 
 // Criando servidor HTTP
 const server = http.createServer((request, response) => {
+  console.log(`Request Method: ${request.method} | Endpoint: ${request.url}`);
+
   // Escrevendo informações no cabeçalho
   response.writeHead(200, { 
     'Content-Type': 'text/html', 
   });
-  response.end('<h1>Hello World!</h1>');
+  response.end('<h1>Ola Mundo!</h1>');
 });
 
 server.listen(3001, () => {
---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Brendo',
+  },
+  {
+    id: 2,
+    name: 'Andrei',
+  },
+  {
+    id: 3,
+    name: 'Maisa',
+  },
+  {
+    id: 4,
+    name: 'Jose',
+  },
+];
