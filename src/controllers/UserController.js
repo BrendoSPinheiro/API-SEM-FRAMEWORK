fc355a061312a50af10483807ed1da6cebb15046
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-22 20:28:54 -0300

    feat: add list users by order

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,20 @@
 const users = require('../mocks/users');
 class UserController {
   listUsers(request, response){
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === 'desc') {
+        return a.id < b.id ? 1 : -1;
+      }
+
+      return a.id > b.id ? 1 : -1;
+    })
+
     response.writeHead(200, { 
       'Content-Type': 'application/json',
     });
-    response.end(JSON.stringify(users));
+    response.end(JSON.stringify(sortedUsers));
   }
 }
 
