25a7d977794b2c4b6867dfa9bd438c8740b63455
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-22 21:01:59 -0300

    feat: add get user by id and validation user not found

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,10 +18,21 @@ class UserController {
   }
 
   getUserById(request, response) {
-    response.writeHead(200, { 
-      'Content-Type': 'application/json',
-    });
-    response.end(JSON.stringify({ ok: true }));
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { 
+        'Content-Type': 'application/json',
+      });
+      response.end(JSON.stringify({ error: 'user not found' }));
+    } else {
+      response.writeHead(200, { 
+        'Content-Type': 'application/json',
+      });
+      response.end(JSON.stringify(user));
+    }
   }
 }
 
