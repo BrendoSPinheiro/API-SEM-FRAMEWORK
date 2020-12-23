eb1a40dd0ede164283df3cf03a5e007578a91b3a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-23 17:54:30 -0300

    feat: add create user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -27,7 +27,28 @@ class UserController {
   }
 
   createUser(request, response) {
-    response.send(200, { ok: true });
+    let body = '';
+
+    request.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    request.on('end', () => {
+      body = JSON.parse(body);
+
+      const lastUserId = users[users.length - 1].id;
+
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+
+      users.push(newUser);
+
+      response.send(200, newUser);
+    });
+
+
   }
 }
 
